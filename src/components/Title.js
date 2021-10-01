e1006e75899bf9ccdd196dbc0f341c01f59a0dbc
uranghishna <hishna.di@gmail.com>
2021-10-01 17:20:32 +0700

    state effect

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import List from './List';
 
 const Title = ({title, alertTitle, profile, profiles}) => {
     alertTitle();
     return (
-        <h1>
+        <div>
+            <h1>
             {JSON.stringify(profile)};
             {JSON.stringify(profiles)};
             Title : {title} || {profile.name} || {profile.location};
-        </h1>
+            </h1>
+            <h1>List nama orang</h1>
+            <ul>{profiles.map((profile) => (
+                <List name={profile.name}/>
+            ))}</ul>
+        </div>
     );
 };
 
