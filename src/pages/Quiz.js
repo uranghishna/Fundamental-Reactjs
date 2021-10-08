0d5bd79efc5100412658db93c8f71fb5a243f272
uranghishna <hishna.di@gmail.com>
2021-10-08 15:28:06 +0700

    select quiz

--- src/pages/Quiz.js
+++ src/pages/Quiz.js
@@ -2,22 +2,62 @@ import React, {useState} from 'react'
 import { quiz } from '../components/quiz/FakeData'
 
 const Quiz = () => {
-    const {id, question, options} = quiz[1];
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const {id, question, options} = quiz[currentIndex];
 
+    const nextQuestion = () =>{
+        if(quiz.length - 1 === currentIndex) return;
+        setCurrentIndex(currentIndex + 1)
+    }
+
+    const previousQuestion = () =>{
+        if(currentIndex === 0) return;
+        setCurrentIndex(currentIndex - 1)
+    }
+    
 
     return (
         <div className="container">
             <h1 className="text-center my-3">Happy Quiz</h1>
+            <div className="card" style={{ backgroundColor: 'grey' }}>
+                <div className="card-body" style={{
+                    display: 'flex',
+                    padding: 10,
+                    flexWrap: 'wrap',
+                }}>
+                    {quiz.map((item, index) => (
+                        <div className="border border-black" style={{
+                        height: 35,
+                        width: 35,
+                        display: 'flex',
+                        justifyContent: 'center',
+                        justifyItems: 'center',
+                        alignItems: 'center',
+                        margin: 5,
+                        borderRadius: 5,
+                        cursor: 'pointer',
+                        backgroundColor: index === currentIndex ? "greenyellow" : "white"
+                        }}
+                        onClick={() => setCurrentIndex(index)}
+                        >
+                        {index + 1}
+                    </div>
+                    ))}
+                </div>
+            </div>
             <div className="card">
-                <div className="card-header">{question}</div>
+                <div className="card-header">{currentIndex +1}. {question}</div>
                 <div className="card-body">
-                    {options.map(item => (
+                    {options.map((item, index) => (
                         <div
                         style={{
                             display: 'flex',
                             justifyItems: 'center',
                             alignItems: 'center',
-                        }}>
+                            alignContent: 'center',
+                        }}
+                        key={index}
+                        >
                             <div
                             style={{
                                 height: 20,
@@ -33,9 +73,9 @@ const Quiz = () => {
                     ))}
                 </div>
             </div>
-            <div className="d-flex justify-content-between py-3">
-                <button className="btn btn-secondary col-sm-2">Previous</button>
-                <button className="btn btn-primary col-sm-2">Next</button>
+            <div className="d-flex justify-content-between pt-3">
+                <button className="btn btn-secondary col-sm-2" onClick={() => previousQuestion()} disabled={currentIndex === 0 ? true : false}>Previous</button>
+                <button className="btn btn-primary col-sm-2" onClick={() => nextQuestion()} disabled={quiz.length - 1 === currentIndex ? true : false}>Next</button>
             </div>
         </div>
     )
